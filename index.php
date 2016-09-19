60732aacb88b27f1cdcca07b07899d317c423fd2
Riddhiman Adib <adib2149@gmail.com>
2016-09-19 12:28:25 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $response = array();
 
 // helper function
 // get data from url
-function getAndProcessData($url) {
+function getAndProcessData($url, $color) {
 
     global $response;
 
@@ -20,6 +20,7 @@ function getAndProcessData($url) {
     $data['link'] = $link;
     $data['pubDate'] = date("d M, Y", strtotime($pubDate));
     $data['allWeeklies'] = $allWeeklies;
+    $data['color'] = $color;
 
     array_push($response, $data);
 }
@@ -27,10 +28,11 @@ function getAndProcessData($url) {
 
 // get ANDROID WEEKLIES data
 $androidWeeklyUrl = "http://us2.campaign-archive1.com/feed?u=887caf4f48db76fd91e20a06d&id=4eb677ad19";
-getAndProcessData($androidWeeklyUrl);
+getAndProcessData($androidWeeklyUrl, 'green');
 
-// get ANDROID WEEKLIES data
+// get KOTLIN WEEKLIES data
 $kotlinWeeklyUrl = "http://us12.campaign-archive1.com/feed?u=f39692e245b94f7fb693b6d82&id=93b2272cb6";
-getAndProcessData($kotlinWeeklyUrl);
+getAndProcessData($kotlinWeeklyUrl, 'light-blue');
+
 
 echo json_encode($response);
